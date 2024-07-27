8365f667fdcea206cbaaa4e04fd7d83e78a29eaa
lutri <lutringerkevin@gmail.com>
2024-07-27 17:26:43 +0200

    correction

---
+++ src/component/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Banner.scss';
+
+const Banner = ({ backgroundImage, text }) => {
+  return (
+    <div className="banner">
+      <img src={backgroundImage} alt="Bannière" className="banner-image" />
+      <div className="banner-text">{text}</div>
+    </div>
+  );
+}; 
+
+export default Banner;
